94016d7f8fc0e8e2eaa353b96f6a9d1d3de39b69
Brian Ashworth <bosrsf04@gmail.com>
2017-01-23 20:37:58 -0500

    possible skeleton base using a painted panel

---
+++ Hnefatafl.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hnefatafl extends JFrame {
+    public static void main(String[] args) {
+        new Hnefatafl();
+    }
+
+    private HnefataflPanel game;
+
+    public Hnefatafl() {
+        // Set window properties
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Copenhagen Hnefatafl 11x11");
+
+        // Create a panel
+        game = new HnefataflPanel();
+        add(game);
+
+        // Set size and show
+        setMinimumSize(new Dimension(480, 640));
+        setSize(new Dimension(570, 750));
+        setVisible(true);
+    }
+}

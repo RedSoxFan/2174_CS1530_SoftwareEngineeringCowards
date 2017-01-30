36506e4c7759ab7b848d8a83eade2d057e8f8576
Aaron Tamenne <aaront2468@gmail.com>
2017-01-30 18:01:53 -0500

    added initial dummy test

---
+++ src/test/java/InitTest.java
@@ -0,0 +1,10 @@
+import cowards.Hnefatafl;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InitTest {
+	@Test
+	public void tempTest() {
+		assertEquals(true, true);
+	}
+}

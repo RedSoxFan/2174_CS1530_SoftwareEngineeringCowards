6295523a6b2581525bae30c5e02dc6df3dffe373
Aaron Tamenne <aaront2468@gmail.com>
2017-02-28 01:25:38 -0500

    updated save test

--- src/test/java/SaveLoadTest.java
+++ src/test/java/SaveLoadTest.java
@@ -1,25 +1,43 @@
 import static org.junit.Assert.*;
 
 import cowards.Board;
+import java.io.*;
+import java.util.*;
 import org.junit.Test;
 
 public class SaveLoadTest {
   /**
-    Test saving a game board in general.
+    Test saving a game board and comparing its saved contents with a known save
+    file that executes the same moves.
    */
   @Test
   public void saveTest() {
     try {
       Board board = new Board();
-      String fileName = "TEST";
+      board.select(3, 0);
+      board.move(3, 4);
+      board.select(5, 3);
+      board.move(5, 2);
+      String fileName = "SAVETEST";
       assertTrue(board.saveBoard(fileName));
+      Scanner saveReader = new Scanner(new File("saved_games/SAVETEST.txt"));
+      Scanner testReader = new Scanner(new File("saved_games/TEST.txt"));
+      
+      while (saveReader.hasNextLine() && testReader.hasNextLine()) {
+        String line1 = saveReader.nextLine();
+        String line2 = testReader.nextLine();
+        
+        if (!line1.equals(line2)) {
+          fail();
+        }
+      }
     } catch (Exception ex) {
       fail();
     }
   }
   
   /**
-    Test loading a game board in general.
+    Test loading a known game save file and ensuring the contents are read properly.
    */
   @Test
   public void loadTest() {
